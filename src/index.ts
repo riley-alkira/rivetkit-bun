67776d68d3c390bed74ebd9b19235c3d76f4263a
Riley Keane <rileykeane98@gmail.com>
2026-03-12 09:40:16 +1000

    Add explicit serve setup

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ const app = new Hono();
 app.use('*', logger())
 
 // Exposes Rivet API to communicate with actors
-app.all("/api/rivet/*", (c) => registry.handler(c.req.raw));
+app.all("/api/rivet/*", async (c) => { 
+  return registry.handler(c.req.raw);
+})
 
-export default app;
+Bun.serve({
+  fetch: app.fetch,
+  port: Number(process.env.PORT ?? 3000),
+  hostname: "0.0.0.0",
+  idleTimeout: 120
+});
